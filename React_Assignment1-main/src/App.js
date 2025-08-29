1a4ad7be8d9b0efa3100d42f72f262fc0a851bbc
Monishksoni <mksoni6354@gmail.com>
2025-08-30 00:23:19 +0530

    Add files via upload

---
+++ React_Assignment1-main/src/App.js
@@ -0,0 +1,15 @@
+// Task:
+// • Set up a new React.js project using create-react-app.
+// • Create a basic component that displays "Hello, React!" on the web page.
+
+import './App.css';
+
+function App() {
+  return (
+   <>
+      <h1>Hello React!</h1>
+   </>
+  );
+}
+
+export default App;
